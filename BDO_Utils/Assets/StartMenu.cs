bcef653ed147e8c39c98ebc18470f2fa846fa1c5
CEJO_C <CEJOGameDevC@gmail.com>
2018-06-19 00:01:15 +0200

    Added script with quit method to the quit button

---
+++ BDO_Utils/Assets/StartMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenu : MonoBehaviour {
+
+    /*Method used to quit the program from the main menu*/
+	public void QuitProgram()
+    {
+        //Tempcode to test quit call
+        Debug.Log("QUITTING PROGRAM!");
+        //End tempcode
+        Application.Quit();
+    }
+}

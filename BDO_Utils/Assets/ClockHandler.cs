623b5f0a4817f052f9d9439f4e07218ea7f7e2f0
user.email <CEJOGameDevJ@hotmail.com>
2018-06-19 10:41:33 +0200

    Made some fixes to the clock

--- BDO_Utils/Assets/ClockHandler.cs
+++ BDO_Utils/Assets/ClockHandler.cs
@@ -11,7 +11,7 @@ public class ClockHandler : MonoBehaviour {
     [HideInInspector] private DateTime m_currentTime;
 
     // The seconds before 15 minutes has passed when the alarm should go off (warn the user)
-    [HideInInspector] private int m_secondsBeforeWarning = 0;
+    [HideInInspector] private int m_secondsBeforeWarning = 15 * 60 - 30;    // Change this just for debug atm
 
     // The seconds before an item appears at the marketplace after it was registered
     [HideInInspector] private int m_secondsAfterItemAppear = 15 * 60;
@@ -22,6 +22,16 @@ public class ClockHandler : MonoBehaviour {
     private void Awake()
     {
         m_alarms = new List<DateTime>();
+        m_currentTime = DateTime.Now;
+
+        // Create a test alarm 10 seconds from now by exakt time
+        //AddAlarmAtSpecificTime(m_currentTime.Hour, m_currentTime.Minute, m_currentTime.Second + 10);
+
+        // Create a test alarm 20 seconds from now
+        //AddAlarmInAmountOfTime(0, 0, 20);
+
+        // Create a test alarm 30 
+        //AddAlarmItemWasRegistered(m_currentTime.Hour, m_currentTime.Minute, m_currentTime.Second);
     }
 	
 	// Update is called once per frame
@@ -33,15 +43,20 @@ public class ClockHandler : MonoBehaviour {
         // Temporary Debug Section
         if(debugShowTimeConsole)
         {
-            Debug.Log(m_currentTime.ToString("HH:mm:ss"));
+            //Debug.Log(m_currentTime.ToString("HH:mm:ss"));
+            Debug.Log(m_currentTime.TimeOfDay);
         }
 
         // Check if an alarm should go off
-        foreach (DateTime time in m_alarms)
+        foreach (DateTime alarmTime in m_alarms)
         {
-            // TODO: Check if an alarm should go off
+            int difference = TimeSpan.Compare(m_currentTime.TimeOfDay, alarmTime.TimeOfDay);
+            if(difference == 0 || difference == 1)
+            {
+                Debug.Log("Time is up!");
+                Debug.Log(m_currentTime.TimeOfDay + " is bigger then " + alarmTime.TimeOfDay);
+            }
         }
-
     }
 
     /* Makes the alarm go off at a specific given time, for example, 22:10:00.
@@ -53,7 +68,8 @@ public class ClockHandler : MonoBehaviour {
      */
     public void AddAlarmAtSpecificTime(int hours, int minutes, int seconds)
     {
-        DateTime newAlarmTime = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, hours, minutes, seconds);
+        DateTime newAlarmTime = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day
+            , hours, minutes, seconds);
         m_alarms.Add(newAlarmTime);
     }
 
@@ -67,9 +83,10 @@ public class ClockHandler : MonoBehaviour {
     public void AddAlarmInAmountOfTime(int hours, int minutes, int seconds)
     {
         // Add a time to track
-        DateTime newAlarmTime = m_currentTime;
-        newAlarmTime.AddMinutes(hours * 60 + minutes);
-        newAlarmTime.AddSeconds(seconds);
+        DateTime newAlarmTime = DateTime.Now;
+        newAlarmTime = newAlarmTime.AddHours(hours);
+        newAlarmTime = newAlarmTime.AddMinutes(minutes);
+        newAlarmTime = newAlarmTime.AddSeconds(seconds);
         m_alarms.Add(newAlarmTime);
     }
 
@@ -82,8 +99,9 @@ public class ClockHandler : MonoBehaviour {
      */
     public void AddAlarmItemWasRegistered(int hours, int minutes, int seconds)
     {
-        DateTime newAlarmTime = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, hours, minutes, seconds);
-        newAlarmTime.AddSeconds(m_secondsAfterItemAppear - m_secondsBeforeWarning);
+        DateTime newAlarmTime = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day
+            , hours, minutes, seconds);
+        newAlarmTime = newAlarmTime.AddSeconds(m_secondsAfterItemAppear - m_secondsBeforeWarning);
         m_alarms.Add(newAlarmTime);
     }
 
